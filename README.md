2ce7a4a51430f22b2b0f712df9147aa36afacd52
Marcus Henrik Simonsen <mhsi@itu.dk>
2022-10-18 20:50:34 +0200

    Final fix

--- README.md
+++ README.md
@@ -6,23 +6,22 @@ Hand-in 2 for Distributed Systems course, ITU SWU 3.sem
 
 ## a) What are packages in your implementation? What data structure do you use to transmit data and meta-data?
 
-We have made a implementation of a package, which is represented by a struct called 'present'. This consist of meta-data (syncronization and acknowledgement keys) and data (the data-field, which consist of an int).
+We have made a implementation of a package, which is represented by a struct called '_package'. This consist of meta-data (syncronization and acknowledgement keys) and data (the data-field, which consist of an int).
 
 ## b) Does your implementation use threads or processes? Why is it not realistic to use threads?
 
-Yes, our implementation uses go routines to simulate the process.
-This is not realistic in a real implementation since the process would happen between different systems across a network.
-Threads does not make sense in
+Our implementation uses go routines (threads) to simulate the process. This is not realistic in a real implementation since the process would happen between different systems across a network. Using goroutines is not a realistic simulation since they are much more stable, than client and servers using the network. Fx, in the real world, a server or client can fail (turn off, crash, etc), where as our threads won't.
 
 ## c) How do you handle message re-ordering?
 
 Re-ordering is when the messages don't arrive in the same order, which they were sent. This can cause problems in multiple cases. One example is if a web server sends an html file to a client. If this message is split up into fragments, reordered and then assembled in an incorrect order, this would lead to big problems.
-We dont handle message re-ordering, since we only send one message at a time. This message also won't be split into fragments, which therefore can't be re-ordered.
+When a package is sent in multiple fragments, we sort the fragments by their sequence number, thereby unsuring that the data is interpreted in the correct order.
 
 ## d) How do you handle message loss?
 
 Message loss is when a message is sent, but never gets received. The problems with this are obvious. This can be caused by multiple things. Device failures, connection loss (physical and wireless), etc. Here it is important that a system won't wait forever until a message arrives, since if the message is lost, the system will halt forever.
-We dont handle message loss, since we have simulated this part of the process.
+We handle message loss by keep sending a package until we get a response. This ensures that the other part has "heard" the message.
+Though there is one case, where a message loss will break the program. This is in the case that the ACK message is lost, when it should be a response to a SYN+ACK message.
 
 ## e) Why is the 3-way handshake important?
 
--- main.go
+++ main.go
@@ -1,76 +1,101 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 	"time"
 )
+
+const (
+	SYNBIT   = 1 << 0
+	ACKBIT   = 1 << 1
+	FINBIT   = 1 << 2
+	DATABIT  = 1 << 3
+	STARTBIT = 1 << 4
+)
+
+func isAppropriateResponse(sent _package, response _package) bool {
+	if sent.dataOffset == 1 { //			Case	(SYN)
+		return response.dataOffset == 3 // 	Response	(SYN+ACK)
+	} else if sent.dataOffset == 3 { //		Case	(SYN+ACK)
+		return response.dataOffset == 2 //	Response	(ACK)
+	} else if sent.dataOffset == 8 { //		Case	(DATA)
+		return response.dataOffset == 10 //	Response	(DATA+ACK)
+	} else if sent.dataOffset == 16 { //	Case	(START)
+		return response.dataOffset == 1 //	Response	(SYN)
+	} else if sent.dataOffset == 4 { //		Case	(FIN)
+		return response.dataOffset == 6 //	Response	(FIN+ACK)
+	} else if sent.dataOffset == 6 { //		Case	(FIN+ACK)
+		return response.dataOffset == 2 //	Response	(ACK)
+	}
+	return false
+}
 
 var requestDone bool = false
 
 func main() {
-	clientAmount := 5
+	rand.Seed(1)
+	clientAmount := 4
 
 	// Initialize channels
 	forwarderChann := make(chan _package)
 	var clientChannels []chan _package
+	simulationCaseChann := make(chan int)
 	for i := 0; i < clientAmount; i++ {
-		clientChannels = append(clientChannels, make(chan _package))
+		clientChannels = append(clientChannels, make(chan _package, 8))
 	}
 
-	var userChannels []chan userRequest
-	for i := 0; i < clientAmount; i++ {
-		userChannels = append(userChannels, make(chan userRequest))
-	}
-
-	// Run goroutines
-	go postNord(forwarderChann, clientChannels)
-
+	// Run goroutines for clients
 	for i, chann := range clientChannels {
-		go client(i, chann, forwarderChann, userChannels[i])
+		go client(i, chann, forwarderChann)
 	}
+
+	go forwarder(forwarderChann, clientChannels, simulationCaseChann)
 
 	// Run user interface
-	userInterface(userChannels)
+	userInterface(forwarderChann, simulationCaseChann)
 
 	// What to simulate (perfect, message loss, re-ordering, etc)
 
 	// Which client to communicate between
-
 }
 
-func client(ip int, inbox chan _package, outgoing chan _package, userInbox chan userRequest) {
+func client(ip int, inbox chan _package, outgoing chan _package) {
+	SEQ := 0
+	ACK := 0
+	FIN := 0
+	var dataPackagesReceived []_package
+	var dataToSend []string
+	dataToSendIdx := 0
 	sendPackage := func(p _package) {
 		outgoing <- p
+		fmt.Println("IP" + fmt.Sprint(ip) + ": Sent package: " + fmt.Sprint(p))
 	}
-	receivePackage := func(p _package) {
-		p = <-inbox
+	receivePackage := func() _package {
+		p := <-inbox
+		fmt.Println("IP" + fmt.Sprint(ip) + ": Received package: " + fmt.Sprint(p))
+		return p
 	}
 
 	exchangePackages := func(p _package) _package {
-		recieved := false
+		var receivedPackage _package
 
 		go func() {
-			for !recieved {
-				rp := <-inbox
-				if p.seq != 0 && p.ack == 0 && p.fin == 0 && p.data == "" { //
-					recieved = rp.seq != 0 && rp.ack != 0 && p.fin == 0 && p.data == ""
-				} else if p.seq != 0 && p.ack != 0 && p.fin == 0 && p.data == "" {
-					recieved = rp.seq == 0 && rp.ack != 0 && p.fin == 0 && p.data == ""
-				} else if p.seq == 0 && p.ack == 0 && p.fin == 0 && p.data != "" {
-					recieved = rp.seq == 0 && rp.ack != 0 && rp.fin == 0 && rp.data != ""
-				} else if p.seq == 0 && p.ack != 0 && rp.fin == 0 && p.data != "" {
-					recieved = rp.seq == 0 && rp.ack != 0 && rp.fin == 0 && rp.data != ""
-				}
+			for (receivedPackage == _package{}) || !isAppropriateResponse(p, receivedPackage) {
+				receivedPackage = receivePackage()
 			}
 		}()
-
-		for !recieved {
-			outgoing <- p
+		for (receivedPackage == _package{}) || !isAppropriateResponse(p, receivedPackage) {
+			sendPackage(p)
 			time.Sleep(10 * time.Millisecond)
 		}
 
-		return p
+		return receivedPackage
 	}
 
 	verifyAck := func(SYN, ACK int) bool {
@@ -78,53 +103,110 @@ func client(ip int, inbox chan _package, outgoing chan _package, userInbox chan 
 	}
 
 	for {
-		select {
-		case request := <-userInbox: // Sender
+		rp := receivePackage()
+		if rp.dataOffset == 16 { // Case 0 (Start)
 
-			// Three-way handshake
-			p := exchangePackages(_package{ip, request.desIP, 1, 0, 0, ""})
+			dataToSend = strings.Split(rp.data, " ")
 
-			outgoing <- _package{ip, request.desIP, 0, p.seq + 1, 0, ""}
-
-			message := request.data
-
-			for i := 0; i < len(message); i++ {
-				seq++
-				outgoing <- _package{ip, request.desIP, 0, seq, 0, string(message[i])}
+			SEQ = rand.Int()
+			var p _package
+			for (p == _package{}) || !verifyAck(SEQ, p.ack) {
+				p = exchangePackages(_package{ip, rp.srcIP, SYNBIT, SEQ, 0, 0, ""})
 			}
-
-		case p := <-inbox: // Receiver
-
-			if p.seq != 0 && p.ack == 0 {
-				p = exchangePackages(_package{ip, p.srcIP, 1, p.seq + 1, 0, ""})
+			inbox <- p
+			//fmt.Println("\tCase 0 completed")
+		} else if rp.dataOffset == 1 { // Case 1 (SYN)
+			SEQ = rand.Int()
+			ACK = rp.seq + 1
+			var p _package
+			for (p == _package{}) || !verifyAck(SEQ, p.ack) {
+				p = exchangePackages(_package{ip, rp.srcIP, SYNBIT | ACKBIT, SEQ, ACK, 0, ""})
 			}
-			if p.seq == 0 && p.ack != 0 {
-
+		} else if rp.dataOffset == 3 { // Case 2 (SYN+ACK)
+			ACK = rp.seq + 1
+			sendPackage(_package{ip, rp.srcIP, ACKBIT, 0, ACK, 0, ""})
+			SEQ++
+			inbox <- exchangePackages(_package{ip, rp.srcIP, DATABIT, SEQ, 0, 0, dataToSend[dataToSendIdx]})
+			dataToSendIdx++
+		} else if rp.dataOffset == 8 { // Case 3 (DATA)
+			if rp.seq == ACK {
+				dataPackagesReceived = append(dataPackagesReceived, rp)
+				ACK++
 			}
+			sendPackage(_package{ip, rp.srcIP, DATABIT | ACKBIT, 0, rp.seq + 1, 0, ""})
+		} else if rp.dataOffset == 10 { // Case 4 + 5 (DATA+ACK)
+			if dataToSendIdx < len(dataToSend) { // Case 4 (DATA+ACK) - send more data
+				SEQ++
+				inbox <- exchangePackages(_package{ip, rp.srcIP, DATABIT, SEQ, rp.seq + 1, 0, dataToSend[dataToSendIdx]})
+				dataToSendIdx++
+				ACK++
+			} else { // Case 5 (DATA+ACK) - send FIN
+				FIN = rand.Int()
+				p := exchangePackages(_package{ip, rp.srcIP, FINBIT, 0, 0, FIN, ""})
+				ACK = p.fin + 1
+				inbox <- p
+			}
+		} else if rp.dataOffset == 4 { // Case 6 (FIN)
+			// Sort packages (message re-ordering)
+			sort.Slice(dataPackagesReceived, func(i, j int) bool { return dataPackagesReceived[i].seq < dataPackagesReceived[j].seq })
 
+			FIN = rand.Int()
+			inbox <- exchangePackages(_package{ip, rp.srcIP, FINBIT | ACKBIT, 0, ACK, FIN, ""})
+			// Print data from packages
+			fmt.Print("IP" + fmt.Sprint(ip) + " has received all data packages: ")
+			for _, p := range dataPackagesReceived {
+				fmt.Print(p.data)
+				fmt.Print(" ")
+			}
+			fmt.Println()
+		} else if rp.dataOffset == 6 { // Case 7 (FIN+ACK)
+			sendPackage(_package{ip, rp.srcIP, ACKBIT, 0, rp.fin + 1, FIN, ""})
+			FIN++
+			requestDone = true
 		}
 	}
-
 }
 
-func postNord(chann chan _package, clientChannels []chan _package) {
+func forwarder(chann chan _package, clientChannels []chan _package, simulationCaseChann chan int) {
+	simulationCase := <-simulationCaseChann
+	fmt.Println("Chosen simulation case: " + fmt.Sprint(simulationCase))
 	for {
 		// Listen for packages
 		p := <-chann
 
 		// Send package to client
 		// Here, message loss, re-ordering, delay, etc will be simulated
-		clientChannels[p.desIP] <- p
+		if simulationCase == 0 || p.dataOffset == 16 {
+			clientChannels[p.desIP] <- p
+		} else if simulationCase == 1 {
+			action := rand.Float32()
+			if action < 0.2 {
+				fmt.Println("Ups, message lost in transport")
+			} else {
+				clientChannels[p.desIP] <- p
+			}
+		} else {
+			panic("Unknown simulation case!")
+		}
 	}
 }
 
 type _package struct {
-	srcIP int
-	desIP int
-	seq   int
-	ack   int
-	fin   int
-	data  string
+	srcIP      int
+	desIP      int
+	dataOffset uint8
+	// 0000 0000
+	//    ^ ^^^^
+	//Start ||||
+	//   Data|||
+	//     FIN||
+	//      ACK|
+	//       SYN
+
+	seq  int
+	ack  int
+	fin  int
+	data string
 }
 
 type userRequest struct {
@@ -134,77 +216,67 @@ type userRequest struct {
 }
 
 // userInterface for managing the program
-func userInterface(chann []chan userRequest) {
+func userInterface(forwarder chan _package, simulationChann chan int) {
 	fmt.Println("Welcome to a TCP/IP simulation")
 	fmt.Println("------------------------------")
+	clientNames := []string{"Thore", "Riko", "Troels", "Rasmus"}
 
-	for {
-		requestDone = false
-		fmt.Println("Please enter a value to start")
-		fmt.Println("1) Start a new simulation")
-		fmt.Println("2) Exit")
+	reader := bufio.NewReader(os.Stdin)
 
-		var input int
-		fmt.Scanln(&input)
+	fmt.Println("Please enter a value to start")
+	fmt.Println("1) Start a new simulation")
+	fmt.Println("2) Exit")
 
-		if input == 2 {
-			os.Exit(3)
-		}
-
-		fmt.Println("Please choose a client to communicate from")
-		fmt.Println("0) Thore")
-		fmt.Println("1) Riko")
-		fmt.Println("2) Troels")
-		fmt.Println("3) Rasmus")
-
-		var from int
-		fmt.Scanln(&from)
-
-		fmt.Println("Please choose a client to communicate to")
-		if from == 0 {
-			fmt.Println("1) Riko")
-			fmt.Println("2) Troels")
-			fmt.Println("3) Rasmus")
-		} else if from == 1 {
-			fmt.Println("0) Thore")
-			fmt.Println("2) Troels")
-			fmt.Println("3) Rasmus")
-		} else if from == 2 {
-			fmt.Println("0) Thore")
-			fmt.Println("1) Riko")
-			fmt.Println("3) Rasmus")
-		} else if from == 3 {
-			fmt.Println("0) Thore")
-			fmt.Println("1) Riko")
-			fmt.Println("2) Troels")
-		}
-
-		var to int
-		fmt.Scanln(&to)
-
-		fmt.Println("Please write a message to send:")
-		var message string
-		fmt.Scanln(&message)
-
-		fmt.Println("Choose simulation cases:")
-		fmt.Println("0) Perfect")
-		fmt.Println("1) Message loss")
-		fmt.Println("2) Re-ordering")
-		fmt.Println("3) Delay")
-		fmt.Println("4) All")
-
-		var simulationCase int
-		fmt.Scanln(&simulationCase)
-
-		fmt.Println("Starting simulation...")
-		fmt.Println("------------------------------")
-
-		newRequest := userRequest{to, message, simulationCase}
-		chann[from] <- newRequest
-
-		for !requestDone {
-			time.Sleep(1 * time.Second)
-		}
+	input, _ := reader.ReadString('\n')
+	input = strings.Trim(input, "\n")
+	if input == "2" {
+		os.Exit(3)
 	}
 
+	fmt.Println("Please choose a client to communicate from")
+	for i, name := range clientNames {
+		fmt.Println(fmt.Sprint(i) + ") " + name)
+	}
+
+	from, _ := reader.ReadString('\n')
+	from = strings.Trim(from, "\n")
+	fromIdx, _ := strconv.Atoi(from)
+	fmt.Println("Sending from: " + fmt.Sprint(fromIdx))
+
+	fmt.Println("Please choose a client to communicate to")
+
+	for i, name := range clientNames {
+		if i == fromIdx {
+			continue
+		}
+		fmt.Println(fmt.Sprint(i) + ") " + name)
+	}
+
+	to, _ := reader.ReadString('\n')
+	to = strings.Trim(to, "\n")
+	toIdx, _ := strconv.Atoi(to)
+
+	fmt.Println("Please write a message to send:")
+	message, _ := reader.ReadString('\n')
+	message = strings.Trim(message, "\n")
+
+	fmt.Println("Choose simulation cases: (NOT IMPLEMENTED!)")
+	fmt.Println("0) Perfect")
+	fmt.Println("1) Message loss")
+
+	simulationCase, _ := reader.ReadString('\n')
+	simulationCase = strings.Trim(simulationCase, "\n")
+	simulationCaseIdx, _ := strconv.Atoi(simulationCase)
+
+	simulationChann <- simulationCaseIdx
+
+	fmt.Println("Starting simulation...")
+	fmt.Println("------------------------------")
+
+	newRequest := _package{toIdx, fromIdx, STARTBIT, 0, 0, 0, message}
+	forwarder <- newRequest
+
+	for !requestDone {
+		time.Sleep(100 * time.Millisecond)
+	}
 }
